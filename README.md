36fe1fe4df8d2c31c21ecbe6298edf7c5f274e23
Thibault Wittemberg <twittemberg@genetec.com>
2020-01-01 12:43:25 -0400

    project: initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Spin.UIKit.Demo
+
+This is a UIKit/Storyboard demo of the usage of the Spin framework.
+
+It implements the three same features but with ReactveSwift / RxSwift / Combine implementations of a Feedback Loop.    
+    
---
+++ Spin.UIKit.Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spin.UIKit.Demo
+//
+//  Created by Thibault Wittemberg on 2020-01-01.
+//  Copyright © 2020 Spinners. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
